63d3b0aac79ec94fc231a5cab2198a0c752d0c56
Emanuel Pinho <ei11152@fe.up.pt>
2013-04-26 01:26:30 +0200

    Update Graph.h

--- src/Graph.h
+++ src/Graph.h
@@ -23,7 +23,7 @@ template<class T> class Graph;
 template<class T>
 class Vertex {
 	T info;
-	vector<Edge<T> > adj;
+	vector<Edge<T> *> adj;
 	bool armazem;
 	bool postoVigilia;
 	int indegree;
@@ -34,6 +34,7 @@ public:
 	friend class Graph<T> ;
 
 	void addEdge(Vertex<T> *dest, double w);
+	bool removeEdgeTo(Vertex<T> *d);
 
 	T getInfo() const;
 
@@ -41,7 +42,7 @@ public:
 	bool getpostoVigilia() const;
 	bool getVisited() const;
 	int getIndegree() const;
-	vector<Edge<T> > getEdges() const;
+	vector<Edge<T> * > getEdges() const;
 
 	void setPostoVigilia(bool vigilia);
 	void setVisited(bool visited);
@@ -59,13 +60,13 @@ T Vertex<T>::getInfo() const {
 }
 
 template<class T>
-vector<Edge<T> > Vertex<T>::getEdges() const {
+vector<Edge<T> *> Vertex<T>::getEdges() const {
 	return adj;
 }
 
 template<class T>
 void Vertex<T>::addEdge(Vertex<T> *dest, double w) {
-	Edge<T> edgeD(dest, w);
+	Edge<T> * edgeD= new Edge<T>(dest, w);
 	adj.push_back(edgeD);
 }
 
@@ -98,6 +99,21 @@ template<class T>
 void Vertex<T>::setVisited(bool visited) {
 	this->visited = visited;
 }
+/*
+template<class T>
+bool Vertex<T>::removeEdgeTo(Vertex<T> *d) {
+	d->indegree--; //adicionado do exercicio 5
+	typename vector<Edge<T> >::iterator it = adj.begin();
+	typename vector<Edge<T> >::iterator ite = adj.end();
+	while (it != ite) {
+		if (it->dest == d) {
+			adj.erase(it);
+			return true;
+		} else
+			it++;
+	}
+	return false;
+}*/
 
 /* ================================================================================================
  * Class Edge
@@ -114,9 +130,8 @@ public:
 	friend class Vertex<T> ;
 
 	double getWeight() const;
-	void setWeight(int peso);
-
 	void decWeight();
+
 	Vertex<T> * getDest() const;
 
 };
@@ -132,25 +147,14 @@ double Edge<T>::getWeight() const {
 }
 
 template<class T>
-void Edge<T>::setWeight(int peso) {
-	this->weight = peso;
-}
-
-template<class T>
-void Edge<T>::decWeight(){
-	cout << "\n\n\nAntes : " << weight;
-	int temp = this->weight;
-	temp--;
-	this->weight = temp;
-	cout << "\n\n\nDepois : " << weight;
+void Edge<T>::decWeight() {
+	weight--;
 }
 
 template<class T>
 Vertex<T> * Edge<T>::getDest() const {
 	return this->dest;
 }
-
-
 
 /* ================================================================================================
  * Class Graph
@@ -178,10 +182,14 @@ public:
 	void cobrirArmazens();
 	void verticesPrioritarios();
 	void adicionarPosto();
-	int verticeComMaisPrioridade(int verticeInfo);
+	int verticeComMaisPrioridade();
 	void preencherVertice(int verticeInfo);
 	void extractToViewer();
 	void extractFinal();
+	bool removeEdge(const T &sourc, const T &dest);
+	void decrementarArestas(Vertex<T> *v);
+	bool arestasDescobertas();
+	int verticesPrioritariosVizinhos(int vertice);
 
 	//exercicio 5
 	Vertex<T>* getVertex(const T &v) const;
@@ -195,16 +203,15 @@ template<class T>
 int Graph<T>::edgeWeight(const T &sourc, const T &dest) {
 
 	typename vector<Vertex<T>*>::iterator it = vertexSet.begin();
-		typename vector<Vertex<T>*>::iterator ite = vertexSet.end();
-		for (; it != ite; it++) {
-			if ((*it)->info == sourc) {
-				for(int i = 0; i < (*it)->getEdges().size(); i++)
-				if (dest == (*it)->getEdges()[i].getDest()->getInfo()){
-					cout << "\n\nO caralho do tamanho e: " << (*it)->getEdges()[i].getWeight() << "\n\n";
-					return ((*it)->getEdges()[i].getWeight());
+	typename vector<Vertex<T>*>::iterator ite = vertexSet.end();
+	for (; it != ite; it++) {
+		if ((*it)->info == sourc) {
+			for (int i = 0; i < (*it)->getEdges().size(); i++)
+				if (dest == (*it)->getEdges()[i]->getDest()->getInfo()) {
+					return ((*it)->getEdges()[i]->getWeight());
 				}
-			}
 		}
+	}
 
 }
 
@@ -214,78 +221,93 @@ int Graph<T>::getNumVertex() const {
 }
 
 template<class T>
-void Graph<T>::extractToViewer(){
+bool Graph<T>::arestasDescobertas() {
 
-
-	GraphViewer *gv = new GraphViewer(600, 600, true, 7772);
-
-		gv->createWindow(600, 600);
-		gv->defineEdgeColor("blue");
-		gv->defineVertexColor("lightGray");
-
-
-		for (int i = 0; i < vertexSet.size(); i++) {
-			int c = vertexSet[i]->getInfo();
-			gv->addNode(c);
-		}
-
-		int arestas = 1;
-		int origem, destino;
-		for (int i = 0; i < vertexSet.size(); i++) {
-			origem = vertexSet[i]->getInfo();
-			for (int c = 0; c < vertexSet[i]->getEdges().size(); c++) {
-				destino = vertexSet[i]->getEdges()[c].getDest()->getInfo();
-				gv->addEdge(arestas, origem, destino, EdgeType::UNDIRECTED);
-				int weight = edgeWeight(origem, destino);
-				if(weight == 2)
-					gv->setEdgeColor(arestas, "red");
-				else
-					gv->setEdgeColor(arestas, "blue");
-				arestas++;
+	typename vector<Vertex<T>*>::iterator it = vertexSet.begin();
+	typename vector<Vertex<T>*>::iterator ite = vertexSet.end();
+	for (; it != ite; it++) {
+		for (int i = 0; i < (*it)->getEdges().size(); i++) {
+			if ((*it)->getEdges()[i]->getWeight() > 0) {
+				return true;
 			}
-
-			gv->rearrange();
 		}
+	}
+
+	return false;
+
 }
 
 template<class T>
-void Graph<T>::extractFinal(){
+void Graph<T>::extractToViewer() {
 
+	GraphViewer *gv = new GraphViewer(600, 600, true, 7772);
+
+	gv->createWindow(600, 600);
+	gv->defineEdgeColor("blue");
+	gv->defineVertexColor("lightGray");
+
+	for (int i = 0; i < vertexSet.size(); i++) {
+		int c = vertexSet[i]->getInfo();
+		gv->addNode(c);
+	}
+
+	int arestas = 1;
+	int origem, destino;
+	for (int i = 0; i < vertexSet.size(); i++) {
+		origem = vertexSet[i]->getInfo();
+		for (int c = 0; c < vertexSet[i]->getEdges().size(); c++) {
+			destino = vertexSet[i]->getEdges()[c]->getDest()->getInfo();
+			gv->addEdge(arestas, origem, destino, EdgeType::UNDIRECTED);
+			int weight = edgeWeight(origem, destino);
+			if (weight == 2)
+				gv->setEdgeColor(arestas, "red");
+			else
+				gv->setEdgeColor(arestas, "blue");
+			arestas++;
+		}
+
+
+
+
+		gv->rearrange();
+	}
+}
+
+template<class T>
+void Graph<T>::extractFinal() {
 
 	GraphViewer *gv2 = new GraphViewer(600, 600, true, 7773);
 
-		gv2->createWindow(600, 600);
-		gv2->defineEdgeColor("blue");
-		gv2->defineVertexColor("lightGray");
+	gv2->createWindow(600, 600);
+	gv2->defineEdgeColor("blue");
+	gv2->defineVertexColor("lightGray");
 
+	for (int i = 0; i < vertexSet.size(); i++) {
+		int c = vertexSet[i]->getInfo();
+		gv2->addNode(c);
+		if (vertexSet[i]->getpostoVigilia()) {
+			gv2->setVertexColor(i + 1, "black");
+		}
+	}
 
-		for (int i = 0; i < vertexSet.size(); i++) {
-			int c = vertexSet[i]->getInfo();
-			gv2->addNode(c);
-			if(vertexSet[i]->getpostoVigilia()){
-				gv2->setVertexColor(i + 1, "black");
-			}
+	int arestas = 1;
+	int origem, destino;
+	for (int i = 0; i < vertexSet.size(); i++) {
+		origem = vertexSet[i]->getInfo();
+		for (int c = 0; c < vertexSet[i]->getEdges().size(); c++) {
+			destino = vertexSet[i]->getEdges()[c]->getDest()->getInfo();
+			gv2->addEdge(arestas, origem, destino, EdgeType::UNDIRECTED);
+			int weight = edgeWeight(origem, destino);
+			if (weight <= 0)
+				gv2->setEdgeColor(arestas, "green");
+			else
+				gv2->setEdgeColor(arestas, "blue");
+			arestas++;
 		}
 
-		int arestas = 1;
-		int origem, destino;
-		for (int i = 0; i < vertexSet.size(); i++) {
-			origem = vertexSet[i]->getInfo();
-			for (int c = 0; c < vertexSet[i]->getEdges().size(); c++) {
-				destino = vertexSet[i]->getEdges()[c].getDest()->getInfo();
-				gv2->addEdge(arestas, origem, destino, EdgeType::UNDIRECTED);
-				int weight = edgeWeight(origem, destino);
-				if(weight <= 0)
-					gv2->setEdgeColor(arestas, "green");
-				else
-					gv2->setEdgeColor(arestas, "blue");
-				arestas++;
-			}
-
-			gv2->rearrange();
-		}
+		gv2->rearrange();
+	}
 }
-
 
 template<class T>
 vector<Vertex<T> *> Graph<T>::getVertexSet() const {
@@ -317,6 +339,31 @@ bool Graph<T>::addVertex(const T &in, bool armazem, bool posto) {
 }
 
 template<class T>
+void Graph<T>::decrementarArestas(Vertex<T> *v) {
+
+	int i = 0;
+	for(int i = 0; i< v->getEdges().size();i++){
+
+		v->getEdges()[i]->decWeight();
+	}
+
+	typename vector<Vertex<T>*>::iterator it2 = vertexSet.begin();
+	typename vector<Vertex<T>*>::iterator ite2 = vertexSet.end();
+
+	for (; it2 != ite2; it2++) {
+		if (v->getInfo() != (*it2)->getInfo()) {
+
+			for( int c = 0; c < (*it2)->getEdges().size(); c++){
+				if ((*it2)->getEdges()[c]->getDest()->getInfo()
+						== v->getInfo()) {
+					(*it2)->getEdges()[c]->decWeight();
+				}
+			}
+		}
+	}
+
+}
+template<class T>
 void Graph<T>::cobrirArmazens() {
 
 	typename vector<Vertex<T>*>::iterator it = vertexSet.begin();
@@ -324,61 +371,105 @@ void Graph<T>::cobrirArmazens() {
 	for (; it != ite; it++) {
 		if ((*it)->getArmazem()) {
 			(*it)->setPostoVigilia(true);
-			for (int i = 0; i < (*it)->getEdges().size(); i++){
-				int peso = (*it)->getEdges()[i].getWeight();
-				peso--;
-				cout << "\n\nllololo: " << peso;
-				(*it)->getEdges()[i].setWeight(peso);
-				//(*it)->getEdges()[i].decWeight();
-				cout << "\n\nlalala: " << (*it)->getEdges()[i].getWeight();
+
+			decrementarArestas((*it));
+
+		}
+	}
+
+}
+
+
+template<class T>
+int Graph<T>::verticesPrioritariosVizinhos(int vertice){
+
+	vector<int> verticesVizinhos;
+	for(int i= 0; i < vertexSet.size(); i++){
+
+		for(int c = 0; c< vertexSet[i]->getEdges().size();c++){
+
+			verticesVizinhos.push_back(vertexSet[c]->getEdges()[c]->getDest()->getInfo());
+		}
+
+		for(int c = 0; c < vertexSet.size(); c++){
+			if(vertexSet[i]->getInfo() != vertexSet[c]->getInfo()){
+				for(int x = 0; x< vertexSet[c]->getEdges().size();x++){
+					if(vertexSet[c]->getEdges()[x]->getDest()->getInfo() == vertexSet[i]->getInfo())
+						if(vertexSet[c]->getEdges()[x]->getDest()->getpostoVigilia())
+							return 0;
+						else
+							verticesVizinhos.push_back(vertexSet[c]->getEdges()[c]->getDest()->getInfo());
+
+				}
 			}
 		}
 	}
+	int maisPrioritario = 999999;
+	for(int i = 0; i < verticesVizinhos.size(); i++){
+		int prioridade = verticesPrioritariosVizinhos(verticesVizinhos[i]);
+		if(prioridade < maisPrioritario)
+			maisPrioritario = prioridade;
+	}
+
+	return 1 + maisPrioritario;
+
 }
 
+
 template<class T>
-int Graph<T>::verticeComMaisPrioridade(int verticeInfo) {
+int Graph<T>::verticeComMaisPrioridade() {
+	int verticeInfo = verticesMaiores[0];
+	int nivel = verticesPrioritariosVizinhos(verticesMaiores[0]);
+	int nivelTemp;
+
+	for(int i = 0; i < verticesMaiores.size(); i++){
+		nivelTemp = verticesPrioritariosVizinhos(verticesMaiores[i]);
+		if(nivelTemp < nivel){
+			verticeInfo =  verticesPrioritariosVizinhos(verticesMaiores[i]);
+			nivel = nivelTemp;
+		}
+	}
 
 
 	return verticeInfo;
 }
 
 template<class T>
-void Graph<T>::preencherVertice(int verticeInfo) {
+void Graph<T>::preencherVertice(int verticeInfo) {	//preenche vertice com posto
+
 	typename vector<Vertex<T>*>::iterator it = vertexSet.begin();
 	typename vector<Vertex<T>*>::iterator ite = vertexSet.end();
-		for (; it != ite; it++) {
-			if((*it)->getInfo() == verticeInfo){
-				(*it)->setPostoVigilia(true);
-				for (int i = 0; i < (*it)->getEdges().size(); i++)
-					(*it)->getEdges()[i].decCobertura();
-				it = vertexSet.end();
-				break;									//saber se e preciso um ou outro, para sair quando encontrar vertice
-			}
+	for (; it != ite; it++) {
+		if ((*it)->getInfo() == verticeInfo) {
+			(*it)->setPostoVigilia(true);
+			decrementarArestas((*it));
+
 		}
+	}
 
 }
 
 template<class T>
-void Graph<T>::adicionarPosto() { //Esta funcao ve quais os vertices com mais arestas por preencher, qual e mais prioritario e coloca posto
+void Graph<T>::adicionarPosto() { //Esta funcao vê qual a lista de vertices iguais .
 
 	verticesPrioritarios();
 
 	while (verticesMaiores.size() > 0) {
-		if (verticesMaiores.size() == 1)
+		if (verticesMaiores.size() == 1) {
 			preencherVertice(verticesMaiores[0]);
-		else
-			for (int i = 0; i < verticesMaiores.size(); i++) {
-				int verticePrioritario = verticeComMaisPrioridade(verticesMaiores[i]);
-				preencherVertice(verticePrioritario);
-			}
+			verticesMaiores.erase(verticesMaiores.begin());
+		} else {
+			int verticePrioritario = verticeComMaisPrioridade();
+			//preencherVertice(verticesMaiores[0]);
+			verticesMaiores.erase(verticesMaiores.begin() + verticePrioritario);
+		}
 
 	}
 }
 
 template<class T>
-void Graph<T>::verticesPrioritarios() { //FAZ RETURN DE UM VETOR DE VERTICES COM O MAIOR NUMERO DE ARESTAS DESCOBERTAS
-
+void Graph<T>::verticesPrioritarios() { //actualiza o vetor valoresMaiores que é um vetor de ints dos vertices com mais arestas descobertas
+	verticesMaiores.clear();
 	int nRuasPorCobrir;
 	int maiorNumero = 0;
 	typename vector<Vertex<T>*>::iterator it = vertexSet.begin();
@@ -386,8 +477,22 @@ void Graph<T>::verticesPrioritarios() { //FAZ RETURN DE UM VETOR DE VERTICES COM
 	for (; it != ite; it++) {
 		nRuasPorCobrir = 0;
 		for (int i = 0; i < (*it)->getEdges().size(); i++)
-			if ((*it)->getEdges()[i].getCobertura() > 0)
+			if ((*it)->getEdges()[i]->getWeight() > 0)
 				nRuasPorCobrir++;
+
+		typename vector<Vertex<T>*>::iterator it2 = vertexSet.begin();
+		typename vector<Vertex<T>*>::iterator ite2 = vertexSet.end();
+
+		for (; it2 != ite2; it2++) {
+			if ((*it)->getInfo() != (*it2)->getInfo()) {
+				for (int i = 0; i < (*it2)->getEdges().size(); i++) {
+					if ((*it2)->getEdges()[i]->getDest()->getInfo()
+							== (*it)->getInfo()
+							&& (*it2)->getEdges()[i]->getWeight() > 0)
+						nRuasPorCobrir++;
+				}
+			}
+		}
 
 		if (nRuasPorCobrir > maiorNumero) {
 			verticesMaiores.clear();
@@ -423,7 +528,6 @@ bool Graph<T>::addEdge(const T &sourc, const T &dest, int w) {
 
 	return true;
 }
-
 template<class T>
 vector<T> Graph<T>::dfs() const {
 	typename vector<Vertex<T>*>::const_iterator it = vertexSet.begin();
@@ -446,7 +550,6 @@ void Graph<T>::dfs(Vertex<T> *v, vector<T> &res) const {
 	typename vector<Edge<T> >::iterator ite = (v->adj).end();
 	for (; it != ite; it++)
 		if (it->dest->visited == false) {
-			//cout << "ok ";
 			dfs(it->dest, res);
 		}
 }

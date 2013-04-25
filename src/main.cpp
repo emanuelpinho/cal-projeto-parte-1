18451342f444ba425fd0a5dfb282621d817cb8f6
Emanuel Pinho <ei11152@fe.up.pt>
2013-04-25 18:27:37 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,12 @@ using namespace std;
 Graph<int> myGraph;
 
 void readFromFile(ifstream &isG) {
-	string tipo, info, armazem, posto, vertice1, vertice2;
+	string tipo, info, armazem, posto, vertice1, vertice2, armazemAresta;
 	bool temArmazem, temPosto;
-	int number, infoVertex1, infoVertex2;
+	int number, infoVertex1, infoVertex2, infoAresta, pesoAresta;
 
 	while (!isG.eof()) {
+
 		getline(isG, tipo);
 
 		if (tipo == "vertice") {
@@ -39,19 +40,25 @@ void readFromFile(ifstream &isG) {
 
 		else if (tipo == "aresta") {
 
+			getline(isG,armazemAresta);
 			getline(isG, vertice1);
 			getline(isG, vertice2);
 
+
 			infoVertex1 = atoi(vertice1.c_str());
 			infoVertex2 = atoi(vertice2.c_str());
+			infoAresta = atoi(armazemAresta.c_str());
 
-			int weight = myGraph.edgeWeight(infoVertex1, infoVertex2);
 
-			if (weight != 2)
-				weight = 1;
+			int weight = 1;
+
+			if (infoAresta == 1 )
+				weight = 2;
+
 
 			myGraph.addEdge(infoVertex1, infoVertex2, weight);
 		} else {
+			break;
 			cout << "Ficheiro num formato errado";
 			exit(1);
 		}
@@ -59,7 +66,7 @@ void readFromFile(ifstream &isG) {
 
 }
 
-void extractToViewer() {
+/*void extractToViewer() {
 
 	GraphViewer *gv = new GraphViewer(600, 600, true, 7772);
 
@@ -72,9 +79,6 @@ void extractToViewer() {
 	for (int i = 0; i < vertices.size(); i++) {
 		int c = vertices[i]->getInfo();
 		gv->addNode(c);
-		if (vertices[i]->getArmazem()) {
-			gv->setVertexColor(i + 1, "blue");
-		}
 	}
 
 	int arestas = 1;
@@ -89,7 +93,7 @@ void extractToViewer() {
 			if(weight == 2)
 				gv->setEdgeColor(arestas, "red");
 			else
-				gv->setEdgeColor(arestas, "yellow");
+				gv->setEdgeColor(arestas, "blue");
 			arestas++;
 		}
 
@@ -97,8 +101,49 @@ void extractToViewer() {
 	}
 
 
-	getchar();
+	//getchar();
 }
+
+void extractFinal() {
+
+	GraphViewer *gv2 = new GraphViewer(600, 600, true, 7773);
+
+	gv2->createWindow(600, 600);
+	gv2->defineEdgeColor("blue");
+	gv2->defineVertexColor("lightGray");
+
+	vector<Vertex<int> *> vertices = myGraph.getVertexSet();
+
+	for (int i = 0; i < vertices.size(); i++) {
+		int c = vertices[i]->getInfo();
+		gv2->addNode(c);
+		if(vertices[i]->getpostoVigilia()){
+			gv2->setVertexColor(i + 1, "black");
+		}
+	}
+
+	int arestas = 1;
+	int origem, destino;
+	for (int i = 0; i < vertices.size(); i++) {
+		origem = vertices[i]->getInfo();
+		vector<Edge<int> > adj2 = vertices[i]->getEdges();
+		for (int c = 0; c < adj2.size(); c++) {
+			destino = adj2[c].getDest()->getInfo();
+			gv2->addEdge(arestas, origem, destino, EdgeType::UNDIRECTED);
+			int weight = myGraph.edgeWeight(origem, destino);
+			if(weight == 1)
+				gv2->setEdgeColor(arestas, "green");
+			else
+				gv2->setEdgeColor(arestas, "blue");
+			arestas++;
+		}
+
+		gv2->rearrange();
+	}
+
+
+
+}*/
 
 int main() {
 
@@ -110,10 +155,14 @@ int main() {
 	}
 
 	readFromFile(isGraph);
-	extractToViewer();
+	myGraph.extractToViewer();
 
-	//myGraph.cobrirArmazens();
+	myGraph.cobrirArmazens();
 
+	myGraph.extractFinal();
+
+
+	getchar();
 
 
 	return 0;
